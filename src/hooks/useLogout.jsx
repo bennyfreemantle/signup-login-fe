5a82ebebe28b0923707126cf606af495427086bc
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-11-20 00:06:53 +0000

    sets todo context to null when a user logs out

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,10 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
+import { useTodoContext } from "./useTodosContext";
 
 export function useLogout() {
   const { dispatch } = useAuthContext();
+  const { dispatch: todoDispatch } = useTodoContext();
   function logout() {
     localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
+
+    // Reset the global context to null when a user logs out
+    // prevents briefly being able to see the previously logged in users info
+    todoDispatch({ type: "SET_TODOS", payload: null });
   }
 
   return { logout };
